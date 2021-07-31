7142a0b04fff5bef53bd5287a56b0ceebabeba66
mikeemorales <j.mikeemorales@gmail.com>
2021-07-31 03:19:01 -0700

    add pages to navbar. Include all routes via controller, routes, and navbar links

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def faq
+  end
+
+  def hiddenstyles
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/about'
+  get 'home/faq'
+  get 'home/hiddenstyles'
+  get '/home/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
